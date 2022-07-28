7fd08bc548246556b1b861e9d4abc5915802f1c0
hao.zhang <hao.zhang@t-systems.com>
2022-07-28 11:04:26 +0200

    create IsPalindrom method with a working approach

--- IsPalindrom/Program.cs
+++ IsPalindrom/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Step 1 - create a new empty string "container"
+//step 2 - read the input string "backwards" and load the characters one by one into the new empty string "container"
+//step 3 - compare if the new string equals to the input string
+
+bool isPalindrom (string stringToTest) 
+{ 
+    //Step 1 - create a new empty string "container"
+    string stringReversed = "";
+    //step 2 - read the input string "backwards" and load the characters one by one into the new empty string "container"
+    for (int i = stringToTest.ToCharArray().Length-1; i >= 0; i--)
+    { 
+        stringReversed += stringToTest.ToCharArray()[i];
+    }
+
+    return stringToTest == stringReversed;
+  
+}
+System.Console.WriteLine(isPalindrom("racecar"));
